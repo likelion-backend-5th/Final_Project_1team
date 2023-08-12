6201e003e715cf55aa9b27324a7d86c1ec68853a
sangdob <51118110+sangdob@users.noreply.github.com>
2023-08-13 00:53:52 +0900

    feat : member entity 추가

---
+++ backend/common/src/main/java/mutsa/common/domain/models/user/Member.java
@@ -0,0 +1,30 @@
+package mutsa.common.domain.models.user;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private final String apiId = UUID.randomUUID().toString();
+
+    @Column(nullable = false)
+    private String nickName;
+
+//    @OneToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+}
