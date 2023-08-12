ffd68a357ea1257aae7890a70260795e851534a3
sangdob <51118110+sangdob@users.noreply.github.com>
2023-08-13 02:07:23 +0900

    feat : report 엔티티 구현

---
+++ backend/common/src/main/java/mutsa/common/domain/models/report/Report.java
@@ -0,0 +1,30 @@
+package mutsa.common.domain.models.report;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import mutsa.common.domain.models.BaseEntity;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(schema = "report")
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Report extends BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "report_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private final String apiId = UUID.randomUUID().toString();
+
+    @Column(nullable = false, columnDefinition = "text", length = 500)
+    private String description;
+}
