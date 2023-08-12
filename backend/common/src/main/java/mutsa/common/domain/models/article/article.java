22fb124f703e83aa9192b4f17badac683901a5a1
sangdob <51118110+sangdob@users.noreply.github.com>
2023-08-13 00:54:25 +0900

    feat : article entity 추가

---
+++ backend/common/src/main/java/mutsa/common/domain/models/article/article.java
@@ -0,0 +1,39 @@
+package mutsa.common.domain.models.article;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import mutsa.common.domain.models.base.BaseEntity;
+import mutsa.common.domain.models.user.User;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "article")
+public class article extends BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "article_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private final String apiId = UUID.randomUUID().toString();
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "text")
+    private String description;
+
+    private String thumbnail;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+}
