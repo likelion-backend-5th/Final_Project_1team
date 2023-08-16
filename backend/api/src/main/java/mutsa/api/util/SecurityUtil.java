dd64cfa9fe4b617577427d31c8b3423184e2f66e
Juhee Park <jouhi3322@gmail.com>
2023-08-16 22:27:01 +0900

    ✨feat : spring security util 유저 UUID를 불러오는 유틸 생성

---
+++ backend/api/src/main/java/mutsa/api/util/SecurityUtil.java
@@ -0,0 +1,30 @@
+package mutsa.api.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.server.ResponseStatusException;
+
+@Slf4j
+public class SecurityUtil {
+    public static String getCurrentUserApiId() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            log.debug("context에 정보가 없습니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "context에 정보가 없습니다.");
+        }
+
+        String username = null;
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+            username = springSecurityUser.getUsername();
+        } else if (authentication.getPrincipal() instanceof String) {
+            username = (String) authentication.getPrincipal();
+        }
+
+        return username;
+    }
+}
