e8435e67b9c67af25a240c0b42cc5c2d523d0a51
Juhee Park <jouhi3322@gmail.com>
2023-08-17 13:43:57 +0900

    🐛fix : SecurityUtil 유저 이름 반환으로 수정

--- backend/api/src/main/java/mutsa/api/service/order/OrderService.java
+++ backend/api/src/main/java/mutsa/api/service/order/OrderService.java
@@ -1,6 +1,5 @@
 package mutsa.api.service.order;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import mutsa.api.dto.order.OrderDetailResponseDto;
 import mutsa.api.dto.order.OrderResponseDto;
@@ -16,33 +15,33 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderService {
     private final UserModuleService userService;
-    private final ArticleModuleService articleService;
+    private final ArticleModuleService articleModuleService;
     private final OrderModuleService orderModuleService;
 
-    public OrderDetailResponseDto findDetailOrder(String articleApiId, String orderUUID, String currentUserApiId) {
-        User user = userService.getByApiId(currentUserApiId);
-        Article article = articleService.getByApiId(articleApiId);
+    public OrderDetailResponseDto findDetailOrder(String articleApiId, String orderApiId, String currentUserApiId) {
+        User user = userService.getByUsername(currentUserApiId);
+        Article article = articleModuleService.getByApiId(articleApiId);
 
-        return orderModuleService.findDetailOrder(article, user, orderUUID);
+        return orderModuleService.findDetailOrder(article, user, orderApiId);
     }
 
     public List<OrderResponseDto> findAllOrder(String articleApiId, String currentUserApiId) {
-        User user = userService.getByApiId(currentUserApiId);
-        Article article = articleService.getByApiId(articleApiId);
+        User user = userService.getByUsername(currentUserApiId);
+        Article article = articleModuleService.getByApiId(articleApiId);
 
         return orderModuleService.findAllOrder(article, user);
     }
 
     public OrderDetailResponseDto saveOrder(String articleApiId, String currentUserApiId) {
-        User user = userService.getByApiId(currentUserApiId);
-        Article article = articleService.getByApiId(articleApiId);
+        User user = userService.getByUsername(currentUserApiId);
+        Article article = articleModuleService.getByApiId(articleApiId);
 
         return orderModuleService.saveOrder(article, user);
     }
 
     public void deleteOrder(String articleApiId, String orderApiId, String currentUserApiId) {
-        User user = userService.getByApiId(currentUserApiId);
-        Article article = articleService.getByApiId(articleApiId);
+        User user = userService.getByUsername(currentUserApiId);
+        Article article = articleModuleService.getByApiId(articleApiId);
 
         orderModuleService.deleteOrder(article, user, orderApiId);
     }
--- backend/api/src/main/java/mutsa/api/util/SecurityUtil.java
+++ backend/api/src/main/java/mutsa/api/util/SecurityUtil.java
@@ -3,15 +3,13 @@ package mutsa.api.util;
 import lombok.extern.slf4j.Slf4j;
 import mutsa.common.exception.BusinessException;
 import mutsa.common.exception.ErrorCode;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 public class SecurityUtil {
-    public static String getCurrentUserApiId() {
+    public static String getCurrentUsername() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null) {
