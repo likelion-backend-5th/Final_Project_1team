b12213a197c18c16f4fde37a517e106456efbf6a
sangdob <51118110+sangdob@users.noreply.github.com>
2023-08-17 18:24:28 +0900

    [feature] OAuth2 config class 작성

---
+++ backend/api/src/main/java/mutsa/api/config/oauth2/OAuth2Config.java
@@ -0,0 +1,25 @@
+package mutsa.api.config.oauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+@Configuration
+public class OAuth2Config {
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+        return new DefaultOAuth2UserService();
+    }
+
+    @Bean
+    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
+        return new HttpSessionOAuth2AuthorizationRequestRepository();
+    }
+}
--- backend/api/src/main/java/mutsa/api/config/security/SecurityConfig.java
+++ backend/api/src/main/java/mutsa/api/config/security/SecurityConfig.java
@@ -8,6 +8,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -20,34 +25,48 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final AuthenticationEntryPoint authenticationEntryPoint;
-
     private final CustomAuthorizationFilter customAuthorizationFilter;
-
+    private final OAuth2UserService<OAuth2UserRequest, OAuth2User> defaultOAuth2UserService;
     private final AuthenticationSuccessHandler authenticationSuccessHandler;
     private final AuthenticationFailureHandler authenticationFailureHandler;
+    private final AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
-                .exceptionHandling(handle ->
-                        handle.authenticationEntryPoint(authenticationEntryPoint))
-                .addFilterBefore(customAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+            .cors(AbstractHttpConfigurer::disable)
+            .exceptionHandling(handle ->
+                handle.authenticationEntryPoint(authenticationEntryPoint))
+            .addFilterBefore(customAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
         httpSecurity.sessionManagement(session ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         httpSecurity.authorizeHttpRequests(registry -> registry.anyRequest().permitAll());
 
+        httpSecurity.oauth2Login(oAuth2LoginConfigurer ->
+            oAuth2LoginConfigurer
+                .authorizationEndpoint(
+                    authorizationEndpointConfig ->
+                        authorizationEndpointConfig.authorizationRequestRepository(
+                                authorizationRequestRepository)
+                            .baseUri("/oauth2/authorize"))
+                .redirectionEndpoint(redirectionEndpointConfig ->
+                    redirectionEndpointConfig.baseUri("/login/oauth2/callback/**"))
+                .userInfoEndpoint(userInfoEndpointConfig ->
+                    userInfoEndpointConfig.userService(defaultOAuth2UserService))
+                .successHandler(authenticationSuccessHandler)
+                .failureHandler(authenticationFailureHandler)
+        );
+
         httpSecurity.formLogin(login ->
-                login.loginProcessingUrl("/api/auth/login")
-                        .successHandler(authenticationSuccessHandler)
-                        .failureHandler(authenticationFailureHandler)
-                        .usernameParameter("username")
-                        .passwordParameter("password")
+            login.loginProcessingUrl("/api/auth/login")
+                .successHandler(authenticationSuccessHandler)
+                .failureHandler(authenticationFailureHandler)
+                .usernameParameter("username")
+                .passwordParameter("password")
         );
 
         return httpSecurity.build();
     }
-
 }
