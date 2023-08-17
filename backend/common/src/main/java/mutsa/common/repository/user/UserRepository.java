b42ee41d5b8b86e9ee9e30cd87fa16c793eec8d0
sangdob <51118110+sangdob@users.noreply.github.com>
2023-08-18 01:45:56 +0900

    [feature] userRepository findByEmail추가 (이후 사용 가능)

--- backend/common/src/main/java/mutsa/common/repository/user/UserRepository.java
+++ backend/common/src/main/java/mutsa/common/repository/user/UserRepository.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByEmail(String email);
+
     Optional<User> findByUsername(String username);
 
     Optional<User> findByApiId(String apiId);
