19a7df6569ad5cecea312baf2029698087a766e1
sangdob <51118110+sangdob@users.noreply.github.com>
2023-08-20 23:21:53 +0900

    test: signup test code 작성

---
+++ backend/api/src/test/java/mutsa/api/controller/user/UserControllerTest.java
@@ -0,0 +1,49 @@
+package mutsa.api.controller.user;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import mutsa.api.dto.user.SignUpUserDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@Transactional
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@ActiveProfiles("test")
+@Slf4j
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UserController userController;
+
+    @Test
+    void signupTest() throws Exception {
+        SignUpUserDto testDto = SignUpUserDto.builder()
+            .username("testuser1")
+            .password("testUser123!@#")
+            .checkPassword("testUser123!@#")
+            .phoneNumber("010-1234-5678")
+            .email("testUse122r@gmail.com")
+            .build();
+
+        String body = new ObjectMapper().writeValueAsString(testDto);
+
+        mockMvc.perform(post("/api/user/signup")
+                .content(body)
+                .contentType(APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+    }
+}
