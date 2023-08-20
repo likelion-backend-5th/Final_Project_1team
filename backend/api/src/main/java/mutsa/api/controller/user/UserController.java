b8bd6ed130287cdd1a71d3e15c228dfdf4dc2895
sangdob <51118110+sangdob@users.noreply.github.com>
2023-08-20 23:10:23 +0900

    [fix] UserController bussiness logic

---
+++ backend/api/src/main/java/mutsa/api/controller/user/UserController.java
@@ -0,0 +1,32 @@
+package mutsa.api.controller.user;
+
+import jakarta.validation.Valid;
+import java.net.http.HttpResponse;
+import lombok.RequiredArgsConstructor;
+import mutsa.api.dto.user.SignUpUserDto;
+import mutsa.api.service.user.UserService;
+import mutsa.api.util.UserUtil;
+import mutsa.common.exception.BusinessException;
+import mutsa.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signUpUser(@RequestBody @Valid SignUpUserDto signUpDto) {
+        if(UserUtil.diffPassword(signUpDto.getPassword(), signUpDto.getCheckPassword())){
+            throw new BusinessException(ErrorCode.DIFFERENT_PASSWORD);
+        }
+        userService.signUp(signUpDto);
+        return new ResponseEntity<>("회원가입이 완료되었습니다.", HttpStatus.NO_CONTENT);
+    }
+}
---
+++ backend/api/src/main/java/mutsa/api/dto/user/SignUpUserDto.java
@@ -0,0 +1,50 @@
+package mutsa.api.dto.user;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import mutsa.common.domain.models.user.User;
+import mutsa.common.domain.models.user.embedded.Address;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpUserDto {
+
+    @NotEmpty
+//    @Pattern(regexp = "^[a-zA-Z0-9],{6,20}$")
+    private String username;
+    @NotEmpty
+//    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\\\d~!@#$%^&*()+|=]{8,16}$")
+    private String password;
+    @NotEmpty
+//    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\\\d~!@#$%^&*()+|=]{8,16}$")
+    private String checkPassword;
+
+    @NotEmpty
+//    @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@\" + \"[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$")
+    private String email;
+
+    @NotEmpty
+    private String phoneNumber;
+
+    private String street;
+    private String zipcode;
+    private String city;
+
+    public static User from(SignUpUserDto signUpUserDto) {
+        User user = User.of(signUpUserDto.getUsername(), signUpUserDto.getPassword(),
+            signUpUserDto.getEmail(), null, null, null);
+        Address address = Address.of(signUpUserDto.getZipcode(), signUpUserDto.getCity(),
+            signUpUserDto.getStreet());
+        user.addAddress(address);
+        return user;
+    }
+}
+
