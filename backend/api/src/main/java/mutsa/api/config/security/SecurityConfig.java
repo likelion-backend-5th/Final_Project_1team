6ba291a4378fea37edbf3bab3d0ed93d6ac15301
kdybe01 <kdybe01@gmail.com>
2023-08-22 08:52:22 +0900

    refactor : cors 설정 WebConfig 추가 - SecurityConfig.WebConfig

--- backend/api/src/main/java/mutsa/api/config/security/SecurityConfig.java
+++ backend/api/src/main/java/mutsa/api/config/security/SecurityConfig.java
@@ -24,6 +24,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
@@ -99,5 +102,18 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Configuration
+    @EnableWebMvc
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173")
+                    .allowedMethods("*")
+                    .allowCredentials(false)
+                    .maxAge(3000);
+        }
     }
 }
