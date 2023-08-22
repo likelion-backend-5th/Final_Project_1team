f9b6e5cb2c7f4731cd3cb9cdf89a389ee5fa1da3
sangdob <51118110+sangdob@users.noreply.github.com>
2023-08-22 16:22:09 +0900

    feature: Exception Global처리 - GlobalExceptionHandler class 추가 - ErrorCode global상태 추가

--- backend/common/src/main/java/mutsa/common/exception/ErrorCode.java
+++ backend/common/src/main/java/mutsa/common/exception/ErrorCode.java
@@ -8,13 +8,19 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     // global
-    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST.value(), "", "잘못된 입력값입니다."),
-    INVALID_TYPE_VALUE(HttpStatus.BAD_REQUEST.value(), "", "잘못된 타입입니다."),
-    SECURITY_CONTEXT_ERROR(HttpStatus.BAD_REQUEST.value(), "", "잘못된 시큐리티내에 정보가 없습니다."),
-    UNKNOWN_ROLE(HttpStatus.NON_AUTHORITATIVE_INFORMATION.value(), "G001", "알수없는 ROLE 입니다."),
-    // auth
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST.value(), "C001", "잘못된 입력값입니다."),
+    INVALID_TYPE_VALUE(HttpStatus.BAD_REQUEST.value(), "C002", "잘못된 타입입니다."),
+    SECURITY_CONTEXT_ERROR(HttpStatus.BAD_REQUEST.value(), "C003", "잘못된 시큐리티내에 정보가 없습니다."),
+    UNKNOWN_ROLE(HttpStatus.NON_AUTHORITATIVE_INFORMATION.value(), "C004", "알수없는 ROLE 입니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED.value(), "C005", " 요청메서드가 허용되지 않습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "C006", "서버에서 오류가 발생했습니다."),
+    HANDLE_ACCESS_DENIED(HttpStatus.FORBIDDEN.value(), "C007", "권한이 없습니다."),
 
-    ACCESS_TOKEN_EXPIRED(HttpStatus.FORBIDDEN.value(), "", "토큰이 만료되었습니다."),
+    // auth
+    ACCESS_TOKEN_EXPIRED(HttpStatus.FORBIDDEN.value(), "A001", "토큰이 만료되었습니다."),
+    REFRESH_TOKEN_NOT_IN_COOKIE(HttpStatus.UNAUTHORIZED.value(), "A002", "쿠키에 refresh-token이 없습니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED.value(), "A003", "쿠키에 저장된 refresh-token이 만료되었거나 인증이 불가능합니다."),
+    USER_TOKEN_NOT_AVAILABLE(HttpStatus.UNAUTHORIZED.value(), "A004", "Access token을 재발급 받을 수 없습니다."),
 
     // user
     USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "USER_NOT_FOUND", "해당 유저를 찾을 수 없습니다."),
