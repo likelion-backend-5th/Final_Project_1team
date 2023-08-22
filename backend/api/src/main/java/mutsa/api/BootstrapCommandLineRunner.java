df3244fc0e9a79a94f20824e4b55b87baff5e361
sangdob <51118110+sangdob@users.noreply.github.com>
2023-08-22 16:48:46 +0900

    fix: dummy data config update

--- backend/api/src/main/java/mutsa/api/BootstrapCommandLineRunner.java
+++ backend/api/src/main/java/mutsa/api/BootstrapCommandLineRunner.java
@@ -3,6 +3,7 @@ package mutsa.api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mutsa.common.BootstrapDataLoader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +12,13 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class BootstrapCommandLineRunner implements CommandLineRunner {
     private final BootstrapDataLoader bootStrapDataLoader;
+    @Value("${dataloader}")
+    private String dataloader;
+
 
     @Override
     public void run(String... args) throws Exception {
+        if(dataloader.equals("1"))
         bootStrapDataLoader.createAdminUser();
     }
 }
