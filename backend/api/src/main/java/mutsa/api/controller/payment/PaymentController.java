fcec6badd0e7df3424fea0782924f4956b55ed4f
kdybe01 <kdybe01@gmail.com>
2023-09-04 22:27:59 +0900

    ✨feat : payment 요청 및 성공, 실패에 따른 컨트롤러 추가 - PaymentController.java

---
+++ backend/api/src/main/java/mutsa/api/controller/payment/PaymentController.java
@@ -0,0 +1,52 @@
+package mutsa.api.controller.payment;
+
+import lombok.RequiredArgsConstructor;
+import mutsa.api.config.payment.TossPaymentConfig;
+import mutsa.api.dto.payment.PaymentDto;
+import mutsa.api.dto.payment.PaymentFailDto;
+import mutsa.api.dto.payment.PaymentSuccessDto;
+import mutsa.api.service.payment.PaymentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/payments")
+public class PaymentController {
+    private final PaymentService paymentService;
+    private final TossPaymentConfig tossPaymentConfig;
+
+    // 결제 요청을 위한 정보 생성 및 반환
+    @GetMapping("/{articleApiId}")
+    public ResponseEntity<PaymentDto> getPaymentInfo(@PathVariable String articleApiId) {
+        PaymentDto paymentDto = paymentService.getPaymentInfoAndSave(articleApiId);
+        return new ResponseEntity<>(paymentDto, HttpStatus.OK);
+    }
+
+    // 결제 성공
+    @GetMapping("/success")
+    public ResponseEntity<PaymentSuccessDto> tossPaymentSuccess(
+            @RequestParam String paymentKey,
+            @RequestParam String orderId,
+            @RequestParam Long amount) {
+        return ResponseEntity.ok().body(paymentService.tossPaymentSuccess(paymentKey, orderId, amount));
+    }
+
+    // 결제 실패
+    @GetMapping("/fail")
+    public ResponseEntity<PaymentFailDto> tossPaymentFail(
+            @RequestParam String code,
+            @RequestParam String message,
+            @RequestParam String orderId
+    ) {
+        paymentService.tossPaymentFail(code, message, orderId);
+        return ResponseEntity.ok().body(
+                PaymentFailDto.builder()
+                        .errorCode(code)
+                        .errorMessage(message)
+                        .orderId(orderId)
+                        .build()
+        );
+    }
+}
